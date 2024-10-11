5351464f63752b378f8cd1b93561d8f164217496
Valentin <valentinignaciopenna@gmail.com>
2024-10-11 18:28:12 -0300

    header y tecnologias

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			background: "#2C002C",
+			primary: "#00B7FF",
+			secondary: "#FF0000",
+		}
 	},
 	plugins: [],
 }
